154a2d98fb147662a7545dee292a375dcb98053c
Raghupathy Sampath <hey.iamraghu@gmail.com>
2022-02-09 21:46:50 +0530

    revisited closures

--- moreFunctions.playground/Contents.swift
+++ moreFunctions.playground/Contents.swift
@@ -102,3 +102,181 @@ enum PasswordError: Error {
     case short
     case obvious
 }
+
+//func checkPassword(_ password: String) throws -> String {
+//    switch password {
+//    case password.count < 5:
+//        throw PasswordError.short
+//    case password == "12345":
+//        throw PasswordError.obvious
+//    case password.count < 8:
+//        return "OK"
+//    case password.count < 10:
+//        return "Good"
+//    default:
+//        return "Excellent"
+//    }
+//}
+
+
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
+
+let password = "1234"
+
+do {
+    let result = try checkPassword(password)
+    print("Feedback on your password: \(result)")
+} catch PasswordError.short {
+    print("Your password is too short")
+} catch PasswordError.obvious {
+    print("Your password is predictable")
+} catch {
+    print("Error with your password selection")
+}
+
+
+//Closures
+
+func greetUser(name: String) {
+    print("Hi \(name)")
+}
+
+var greetCopy = greetUser
+greetCopy("raghu")
+
+
+func getUserData(for id: Int) -> String {
+    if id == 1989 {
+        return "Taylor Swift"
+    } else {
+        return "Anonymous"
+    }
+}
+
+var userDataCopy = getUserData
+print(userDataCopy(1989))
+
+
+let sayHello = { (name: String) -> String in
+    return "hi \(name)"
+}
+
+sayHello("raghu")
+
+
+let team = ["Raja", "Raghu", "Guna", "Vicky", "Selvam", "Dhinesh"]
+let sortedTeam = team.sorted()
+print(sortedTeam)
+
+
+func captainFirstSorted(name1: String, name2: String) -> Bool {
+    if name1 == "Raja" {
+        return true
+    } else if name2 == "Raja" {
+        return false
+    }
+    return name1 < name2
+}
+
+let captainFirstTeam = team.sorted(by: captainFirstSorted)
+print(captainFirstTeam)
+
+
+let captainFirstWithClosure = team.sorted { name1, name2 in
+    if name1 == "Raja" {
+        return true
+    } else if name2 == "Raja" {
+        return false
+    }
+    return name1 < name2
+}
+print(captainFirstWithClosure)
+
+
+
+//Closure - one parameter, returns nothing
+
+let payment = { (user: String) in
+    print("Hi \(user)")
+}
+
+//Closure - one parameter, returns a value
+
+let payment1 = { (user: String) -> String in
+    return "hi \(user)"
+}
+
+//Closure - no parameter, returns a value
+
+let payment2 = { () -> String in
+    return "hi"
+}
+
+
+let rOnly = team.filter { teamMember in
+    teamMember.hasPrefix("R")
+}
+print(rOnly)
+
+
+let uppercaseTeam = team.map { teamMember in
+    teamMember.uppercased()
+}
+
+
+
+func makeArray(size: Int, using generator: () -> Int) -> [Int] {
+    var numbers = [Int]()
+    
+    for _ in 0..<size {
+        let newNumber = generator()
+        numbers.append(newNumber)
+    }
+    
+    return numbers
+}
+
+makeArray(size: 5) {
+    Int.random(in: 1...10)
+}
+
+func generateNumber() -> Int {
+    Int.random(in: 1...40)
+}
+
+makeArray(size: 10, using: generateNumber)
+
+
+
+func doSomething(first: () -> Void, second: () -> Void, third: () -> Void) {
+    print("About to start the first task")
+    first()
+    print("About to start the second task")
+    second()
+    print("About to start the third task")
+    third()
+    print("Done!")
+}
+
+doSomething {
+    print("First task is completed")
+} second: {
+    print("Second task is completed")
+} third: {
+    print("Third task is completed")
+}
+
